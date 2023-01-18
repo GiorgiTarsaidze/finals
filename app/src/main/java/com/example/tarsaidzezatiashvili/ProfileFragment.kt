e7c593225f085d743122e3842b9f26eccb49eb26
GiorgiTarsaidze <117092635+GiorgiTarsaidze@users.noreply.github.com>
2023-01-18 13:32:05 +0400

    Update ProfileFragment.kt

--- app/src/main/java/com/example/tarsaidzezatiashvili/ProfileFragment.kt
+++ app/src/main/java/com/example/tarsaidzezatiashvili/ProfileFragment.kt
@@ -7,7 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.example.myapplication.starter
+import com.example.tarsaidzezatiashvili.starter
 
 
 class ProfileFragment : Fragment() {
